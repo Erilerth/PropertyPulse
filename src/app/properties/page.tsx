5f2c5a8c84edad188e1c73e75c9c4b747d38bea5
Tony THERLIER <146343369+Erilerth@users.noreply.github.com>
2024-06-25 05:40:06 +0200

    setup + Start the Navabar Component

---
+++ src/app/properties/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Properties() {
+  return (
+    <>
+      <h1 className='text-3xl'>properties</h1>
+      <Link href='/'>Go Home</Link>
+    </>
+  );
+}
